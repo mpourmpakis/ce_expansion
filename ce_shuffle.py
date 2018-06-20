ea0637164202c828436d4bae43aa6b2690e21c0a
Jonaxan <jwe10@pitt.edu>
2018-06-20 18:45:09 -0400

    Implemented Min, max, and stdev printing and file writing

--- ce_shuffle.py
+++ ce_shuffle.py
@@ -15,6 +15,11 @@ def atomchanger(nanop, el1, el2):
 
     totalloops = 5
     cemeanlist = [None]*21
+    stdevlist = [None]*21
+    mincelist = [None]*21
+    maxcelist = [None]*21
+    minatomlist = [None]*21
+    maxatomlist = [None] * 21
     k=0
 
     for j in range(0, 101, 5):
@@ -29,11 +34,31 @@ def atomchanger(nanop, el1, el2):
 
         modlist = list(nanoplist)
         celist = [None] * totalloops
+        j = 0
         for i in range(0, totalloops):
             shuffle(modlist)
             nanop.set_chemical_symbols(modlist)
             celist[i] = ce_calc.calculate_CE(nanop)
+            if j == 0:
+                maxce = celist[i]
+                mince = celist[i]
+                minatom = nanop
+                maxatom = nanop
+                j = 1
+            else:
+                if celist[i] < mince:
+                    mince = celist[i]
+                    minatom = nanop
+                elif celist[i] > maxce:
+                    maxce = celist[i]
+                    maxatom = nanop
+
             if i == totalloops - 1:
                 cemeanlist[k] = numpy.mean(celist)
+                stdevlist[k] = numpy.std(celist)
+                mincelist[k] = mince
+                maxcelist[k] = maxce
+                minatomlist[k] = minatom
+                maxatomlist[k] = maxatom
                 k += 1
-    return cemeanlist
+    return cemeanlist, stdevlist, totalloops, mincelist, maxcelist, minatomlist, maxatomlist
