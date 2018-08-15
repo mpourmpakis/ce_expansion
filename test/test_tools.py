79c3ec6914eba8c578084fc821d62f0f25dff55e
jrd101 <jrd101@login1.crc.pitt.edu>
2018-08-14 20:24:17 -0400

    Add tools test

--- test/test_tools.py
+++ test/test_tools.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python
 
+import unittest
+import pickle
+import tools.gen_coeffs
+import test
+
+class TestTools(unittest.TestCase):
+  def setUp(self):
+    with open(test.PRECOMP_COEFFS,"r") as precomp:
+      self.precomputed_coeffs = pickle.load(precomp)
+  def test_coeffs(self):
+    print "\nTesting precomputed bond coeffs versus those prodced by gen_coeffs.py..."
+    try:
+      self.assertEqual(self.precomputed_coeffs, tools.gen_coeffs.precomputed_coeffs)
+      print "Values verified."
+    except AssertionError:
+      print "Values from current script differ from those found in pickle." 
+
+if __name__ == "__main__":
+    unittest.main()
 
