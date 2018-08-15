d81e09a5ed700e69e7342d0894f9af9a3ec24cf9
jrd101 <jrd101@login1.crc.pitt.edu>
2018-08-14 20:25:47 -0400

    Add test test

---
+++ test/test_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os
+import unittest
+import test
+
+class testTest(unittest.TestCase):
+  def testFiles(self):
+    print "\nPerforming meta-test. Testing that test files exist...."
+    for filename in ("test/test_project_structure.py",
+                     "test/test_src.py",
+                     "test/test_tools.py",
+                     test.PRECOMP_COEFFS):
+      try:
+        self.assertTrue(os.path.exists(filename))
+        print filename + " exists."
+      except AssertionError:
+        print filename + " does not exist."
+        raise
+
+if __name__ == "__main__":
+  unittest.__main__()
