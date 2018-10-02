205595ca3fdcb4b3ce1d8025020afcc7e4e4aedd
AcylSilane <jrd101@pitt.edu>
2018-10-02 19:35:59 -0400

    Auto-format

--- src/cn_counter.py
+++ src/cn_counter.py
@@ -1,45 +1,42 @@
 import ase.io
 import ase.neighborlist
-import numpy as np 
+import numpy as np
 from pandas import DataFrame
 
-k=0
-perc_Cu_surface=[None]*21
-Ag_Composition=[None]*21
-Au_Composition=[None]*21
-for a in range(0,101,5):
-    name ="CuboctahedronShell_#3Ag"+str((100-a))+"Au"+str(a)+"min.xyz"
+k = 0
+perc_Cu_surface = [None] * 21
+Ag_Composition = [None] * 21
+Au_Composition = [None] * 21
+for a in range(0, 101, 5):
+    name = "CuboctahedronShell_#3Ag" + str((100 - a)) + "Au" + str(a) + "min.xyz"
     x = ase.io.read(name)
-    atom_size=len(x)
-    y=ase.neighborlist.neighbor_list("i",x,3)
-    symbols=x.get_chemical_symbols()
-    z= zip(symbols,np.bincount(y))
-    surface_atoms=[]
-    cu_surface=[]
-    ag_surface=[]
-    for i in range(0,len(np.bincount(y))):
-        if np.bincount(y)[i]!=12: 
+    atom_size = len(x)
+    y = ase.neighborlist.neighbor_list("i", x, 3)
+    symbols = x.get_chemical_symbols()
+    z = zip(symbols, np.bincount(y))
+    surface_atoms = []
+    cu_surface = []
+    ag_surface = []
+    for i in range(0, len(np.bincount(y))):
+        if np.bincount(y)[i] != 12:
             surface_atoms.append(z[i])
-        
-        
-    for j in range(0,len(surface_atoms)):        
-        if surface_atoms[j][0]=='Cu':
-                cu_surface.append(surface_atoms[j])
+
+    for j in range(0, len(surface_atoms)):
+        if surface_atoms[j][0] == 'Cu':
+            cu_surface.append(surface_atoms[j])
         else:
-                ag_surface.append(surface_atoms[j])
-    Ag_Composition[k]=100-a
-    Au_Composition[k]=a
-    perc_Cu_surface[k]=float(len(cu_surface))*100/float((len(cu_surface)+len(ag_surface)))
-    k=k+1
-    #print len(surface_atoms)
-    
+            ag_surface.append(surface_atoms[j])
+    Ag_Composition[k] = 100 - a
+    Au_Composition[k] = a
+    perc_Cu_surface[k] = float(len(cu_surface)) * 100 / float((len(cu_surface) + len(ag_surface)))
+    k = k + 1
+    # print len(surface_atoms)
+
 print(perc_Cu_surface)
 
-df = DataFrame({'Number of Atoms': atom_size,'Cu Percentage': Cu_Composition, 'Ag Percentage': Ag_Composition, 
-                     'Perc Cu on Surface': perc_Cu_surface})
-
+df = DataFrame({'Number of Atoms': atom_size, 'Cu Percentage': Cu_Composition, 'Ag Percentage': Ag_Composition,
+                'Perc Cu on Surface': perc_Cu_surface})
 
 df.to_excel('147_surface_perc_cu_cubocta_CuAg.xlsx', sheet_name='sheet1',
-    columns=['Number of Atoms', 'Cu Percentage', 'Ag Percentage',
-                'Perc Cu on Surface'], index=False)
-
+            columns=['Number of Atoms', 'Cu Percentage', 'Ag Percentage',
+                     'Perc Cu on Surface'], index=False)
--- src/main.py
+++ src/main.py
@@ -2,15 +2,15 @@
 import structure_gen
 import time
 import ase.io
-from pandas import DataFrame 
+from pandas import DataFrame
 from math import floor
 
- 
+
 # shell_nums is a list with the shell sizes
 def structure(shape, shell_nums):
     atoms_list = []
     for s in shell_nums:
-        if shape in['Icosahedron','Cuboctahedron']:
+        if shape in ['Icosahedron', 'Cuboctahedron']:
             shell_args = [s]
         elif shape in ['Decahedron', 'Lattice', 'Sphere']:
             shell_args = [s] * 3
@@ -23,8 +23,9 @@ def structure(shape, shell_nums):
 
     return atoms_list
 
-#skeleton = ase.Atoms()
-#for composition in range(5,80,2):
+
+# skeleton = ase.Atoms()
+# for composition in range(5,80,2):
 #    random_NP = randomizer(skeleton, "Cu", "Au", composition)
 #    calculate_CE(random_NP)
 
@@ -34,14 +35,18 @@ def randomizer(atoms):
     """
 
     t = time.clock()
-    meancelist1, stdlist1, totalloops, mincelist1, maxcelist1, minatomlist1, maxatomlist1 = ce_shuffle.atomchanger(atoms, 'Cu', 'Ag')
-    #meancelist1, stdlist1, totalloops, mincelist1, maxcelist1, minatomlist1, maxatomlist1 = ce_shuffle.atomchanger(atoms, element1, element2)
+    meancelist1, stdlist1, totalloops, mincelist1, maxcelist1, minatomlist1, maxatomlist1 = ce_shuffle.atomchanger(
+        atoms, 'Cu', 'Ag')
+    # meancelist1, stdlist1, totalloops, mincelist1, maxcelist1, minatomlist1, maxatomlist1 = ce_shuffle.atomchanger(atoms, element1, element2)
     t2 = time.clock()
     print('1= ' + str(t2 - t))
-    meancelist2, stdlist2, totaloops, mincelist2, maxcelist2, minatomlist2, maxatomlist2 = ce_shuffle.atomchanger(atoms, 'Au', 'Cu')
+    meancelist2, stdlist2, totaloops, mincelist2, maxcelist2, minatomlist2, maxatomlist2 = ce_shuffle.atomchanger(atoms,
+                                                                                                                  'Au',
+                                                                                                                  'Cu')
     t2 = time.clock()
     print('2= ' + str(t2 - t))
-    meancelist3, stdlist3, totalloops, mincelist3, maxcelist3, minatomlist3, maxatomlist3= ce_shuffle.atomchanger(atoms, 'Ag', 'Au')
+    meancelist3, stdlist3, totalloops, mincelist3, maxcelist3, minatomlist3, maxatomlist3 = ce_shuffle.atomchanger(
+        atoms, 'Ag', 'Au')
     t2 = time.clock()
     meancelist = meancelist1 + meancelist2 + meancelist3
     mincelist = mincelist1 + mincelist2 + mincelist3
@@ -62,7 +67,7 @@ def fileprint(morphlist, morphologyname, shellnums, atomform, standarddev, smpnu
     el2list.extend(['Cu'] * 21)
     el2list.extend(['Au'] * 21)
     formula = []
-    smplist = [smpnum]*63*len(shellnums)
+    smplist = [smpnum] * 63 * len(shellnums)
     for i in range(0, len(shellnums)):
         formula.extend([atomform[i]] * 63)
 
@@ -85,17 +90,15 @@ def fileprint(morphlist, morphologyname, shellnums, atomform, standarddev, smpnu
 
     df = DataFrame({'Element 1': el1list, 'Percentage 1': perclist1, 'Element 2': el2list, 'Percentage 2': perclist2,
                     'Average Cohesive Energy': morphlist, 'Number of Atoms': formula, 'Standard Deviation': standarddev
-                    , '# of Samples': smplist, 'Minimum CE': mincelist, 'Maximum CE': maxcelist})
+                       , '# of Samples': smplist, 'Minimum CE': mincelist, 'Maximum CE': maxcelist})
 
     df.to_excel(morphologyname + '.xlsx', sheet_name='sheet1',
                 columns=['Element 1', 'Percentage 1', 'Element 2', 'Percentage 2',
-                         'Number of Atoms', 'Average Cohesive Energy','Minimum CE', 'Maximum CE', 'Standard Deviation',
+                         'Number of Atoms', 'Average Cohesive Energy', 'Minimum CE', 'Maximum CE', 'Standard Deviation',
                          '# of Samples'], index=False)
 
 
-
 def boundce(minatomlist, maxatomlist, shellnums, morphologyname):
-
     perclist1 = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100] * 3 * len(shellnums)
     el1list = ['Cu'] * 21
     el1list.extend(['Au'] * 21)
@@ -105,15 +108,15 @@ def boundce(minatomlist, maxatomlist, shellnums, morphologyname):
     el2list.extend(['Au'] * 21)
     el1list.extend(el1list * (len(shellnums) - 1))
     el2list.extend(el2list * (len(shellnums) - 1))
-    k=1
-    for i in range(0, 63*len(shellnums)):
-        k = shellnums[int(floor(i/63))]
-        namemin = morphologyname + 'Shell_#' + str(k) + str(el1list[i]) + str(perclist1[i]) + str(el2list[i]) + str(100 - perclist1[i]) + 'min.xyz'
-        namemax = morphologyname + 'Shell_#' + str(k) + str(el1list[i]) + str(perclist1[i]) + str(el2list[i]) + str(100 - perclist1[i]) + 'max.xyz'
+    k = 1
+    for i in range(0, 63 * len(shellnums)):
+        k = shellnums[int(floor(i / 63))]
+        namemin = morphologyname + 'Shell_#' + str(k) + str(el1list[i]) + str(perclist1[i]) + str(el2list[i]) + str(
+            100 - perclist1[i]) + 'min.xyz'
+        namemax = morphologyname + 'Shell_#' + str(k) + str(el1list[i]) + str(perclist1[i]) + str(el2list[i]) + str(
+            100 - perclist1[i]) + 'max.xyz'
         ase.io.write(namemin, minatomlist[i], format=None, parallel=True, append=False)
         ase.io.write(namemax, maxatomlist[i], format=None, parallel=True, append=False)
-
-
 
 
 def main():
@@ -145,5 +148,6 @@ def main():
     print('Done')
     fileprint(morphlist, morphologyname, shellnums, atomform, standarddev, smpnum, mincelist, maxcelist)
     boundce(minatomlist, maxatomlist, shellnums, morphologyname)
+
 
 main()
