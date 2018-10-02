dcef6f9bc132a5dd4a1e20b874334cd96e8622b2
AcylSilane <jrd101@pitt.edu>
2018-10-02 17:51:29 -0400

    Auto-format the file

--- tools/gamma_gen.py
+++ tools/gamma_gen.py
@@ -47,9 +47,10 @@ def calculate_gamma(element1: str,
     :type element1: str
     :param element2: The second element in the bimetallic pair
     :type element2: str
-    :param exp: Experimental bond dissociation energies. This is the preferred source of data.
+    :param exp: Experimental heterolytic bond dissociation energies. This is the preferred source of data.
     :type exp: str
-    :param est: Theoretical bond dissociation energies. If no experiment is available, we get our data from here.
+    :param est: Theoretical heterolytic bond dissociation energies. If no experiment is available, we get our data from
+                here.
     :type est: str
 
     :return: The two gamma coefficients as two floats inside a tuple, in the order the two elements were provided.
@@ -61,19 +62,24 @@ def calculate_gamma(element1: str,
     if element1 == element2:
         return 1.0, 1.0
 
-    # Get our bond dissociation energies into a dictionary and put them into a set of variables
+    # Get our (heterolytic) bond dissociation energies into a dictionary and put them into a set of variables
     bde_table = csv_to_dict(exp)
     if bde_table[element1][element2] is None:
         bde_table = csv_to_dict(est)
     elif bde_table[element1][element1] is None or bde_table[element2][element2] is None:
         bde_table = csv_to_dict(est)
+    assert (bde_table[element1][element2] is not None)
     bde_mono1 = float(bde_table[element1][element1])  # Element1 - Element1 bond dissociation energy
     bde_mono2 = float(bde_table[element2][element2])  # Element2 - Element2 bond dissociation energy
     bde_hetero = float(bde_table[element1][element2])  # Element1 - Element2 bond dissociation energy
 
-    # Solve the system of linear equations
+    # Set up a system of linear equations to solve for gama.
+    # gamma_1 * bde_metal1_metal1 + gamma_2 * bde_metal2_metal2 = 2 * bde_metal1_metal2
+    # gamma1 + gamma2 = 2
     # These equations come from Equations 5 and 6 in the Bond-Centric Model of Yan et. al.
-    gamma_values = np.linalg.solve([[bde_mono1, bde_mono2], [1, 1]],
-                                   [[2 * bde_hetero], [2]])
+    gamma_values = np.linalg.solve([[bde_mono1, bde_mono2],
+                                    [1, 1]],
+                                   [[2 * bde_hetero],
+                                    [2]])
 
-    return float(gamma_values[0]), float(gamma_values[1])
+    return float(gamma_values[0]), float(gamma_values[1]), bde_table
