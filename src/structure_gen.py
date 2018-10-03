f3ed4f75fe3f07246cb6d06ad79d369589f443df
AcylSilane <jrd101@pitt.edu>
2018-10-03 18:40:51 -0400

    Remove redundant structural generation methods. Only the lattice and sphere have any special parameters here.

--- src/structure_gen.py
+++ src/structure_gen.py
@@ -4,36 +4,20 @@
 import ase.cluster
 
 
-def check_distance(atoms, radius, index):
-    distance = atoms.get_distance(-1, index)
-    print(distance)
-    if distance <= radius and distance != 0:
-        return True
-    else:
-        return False
+def create_lattice(parameters):
+    atoms = ase.cluster.FaceCenteredCubic('Cu', ((1, 0, 0), (0, 1, 0), (0, 0, 1)), parameters)
+    return atoms
 
 
-# structures
-def structuregen(shape, parameters):
-    if shape == "Icosahedron":
-        atoms = ase.cluster.Icosahedron('Cu', noshells=parameters[0])
-        return atoms
-    elif shape == "Decahedron":
-        atoms = ase.cluster.Decahedron('Cu', p=parameters[0], q=parameters[1], r=parameters[2])
-        return atoms
-    elif shape == "Lattice":
-        atoms = ase.cluster.FaceCenteredCubic('Cu', ((1, 0, 0), (0, 1, 0), (0, 0, 1)), parameters)
-        return atoms
-    elif shape == "Sphere":
-        atoms = ase.cluster.FaceCenteredCubic('Cu', ((1, 0, 0), (0, 1, 0), (0, 0, 1)), parameters)
-        cen = atoms.positions.mean(0)
-        radius = (parameters[0] - 1) * 3.61 / 2
-        atoms.append(ase.Atom("Cu", cen))
-        # Filter
-        # 1st arg is a function returning T or F
-        #    Check_distance needs 3 args: atoms object, radius, and a count
-        # The rest of the arguments are lists of things that get plugged in
+def create_sphere(parameters):
+    atoms = ase.cluster.FaceCenteredCubic('Cu', ((1, 0, 0), (0, 1, 0), (0, 0, 1)), parameters)
+    cen = atoms.positions.mean(0)
+    radius = (parameters[0] - 1) * 3.61 / 2
+    atoms.append(ase.Atom("Cu", cen))
+    # Filter
+    # 1st arg is a function returning T or F
+    #    Check_distance needs 3 args: atoms object, radius, and a count
+    # The rest of the arguments are lists of things that get plugged in
 
-        my_cluster = ase.Atoms(filter(lambda x: check_distance(atoms, radius, x.index), atoms))
-        print(my_cluster)
-        return my_cluster
+    my_cluster = 0#ase.Atoms(filter(lambda x: check_distance(atoms, radius, x.index), atoms))
+    return my_cluster
