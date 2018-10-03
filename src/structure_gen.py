62c1f5019ea0d98a47c6674bf86ea78502c5ebf7
AcylSilane <jrd101@pitt.edu>
2018-10-03 18:29:44 -0400

    Clean up inputs; make it explicit where we get these objects from

--- src/structure_gen.py
+++ src/structure_gen.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
+# TODO: Clean this code up, make it more readable
 
-from ase import Atoms, Atom
-from ase.cluster.cubic import FaceCenteredCubic
-from ase.cluster.decahedron import Decahedron
-from ase.cluster.icosahedron import Icosahedron
+import ase.cluster
 
 
-# for sphere
 def check_distance(atoms, radius, index):
     distance = atoms.get_distance(-1, index)
     print(distance)
@@ -19,24 +16,24 @@ def check_distance(atoms, radius, index):
 # structures
 def structuregen(shape, parameters):
     if shape == "Icosahedron":
-        atoms = Icosahedron('Cu', noshells=parameters[0])
+        atoms = ase.cluster.Icosahedron('Cu', noshells=parameters[0])
         return atoms
     elif shape == "Decahedron":
-        atoms = Decahedron('Cu', p=parameters[0], q=parameters[1], r=parameters[2])
+        atoms = ase.cluster.Decahedron('Cu', p=parameters[0], q=parameters[1], r=parameters[2])
         return atoms
     elif shape == "Lattice":
-        atoms = FaceCenteredCubic('Cu', ((1, 0, 0), (0, 1, 0), (0, 0, 1)), parameters)
+        atoms = ase.cluster.FaceCenteredCubic('Cu', ((1, 0, 0), (0, 1, 0), (0, 0, 1)), parameters)
         return atoms
     elif shape == "Sphere":
-        atoms = FaceCenteredCubic('Cu', ((1, 0, 0), (0, 1, 0), (0, 0, 1)), parameters)
+        atoms = ase.cluster.FaceCenteredCubic('Cu', ((1, 0, 0), (0, 1, 0), (0, 0, 1)), parameters)
         cen = atoms.positions.mean(0)
         radius = (parameters[0] - 1) * 3.61 / 2
-        atoms.append(Atom("Cu", cen))
+        atoms.append(ase.Atom("Cu", cen))
         # Filter
         # 1st arg is a function returning T or F
         #    Check_distance needs 3 args: atoms object, radius, and a count
         # The rest of the arguments are lists of things that get plugged in
 
-        my_cluster = Atoms(filter(lambda x: check_distance(atoms, radius, x.index), atoms))
+        my_cluster = ase.Atoms(filter(lambda x: check_distance(atoms, radius, x.index), atoms))
         print(my_cluster)
         return my_cluster
