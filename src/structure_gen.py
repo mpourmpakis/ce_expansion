8997c70d7411eb4d08b1efec8e591321ce1b060f
AcylSilane <jrd101@pitt.edu>
2018-10-03 19:41:25 -0400

    Cube generation now properly works, with CN3 atoms on the corners.

--- src/structure_gen.py
+++ src/structure_gen.py
@@ -1,23 +1,46 @@
 #!/usr/bin/env python3
 # TODO: Clean this code up, make it more readable
 
-import ase.cluster
+import ase.cluster, ase.lattice
 
 
-def create_lattice(parameters):
-    atoms = ase.cluster.FaceCenteredCubic('Cu', ((1, 0, 0), (0, 1, 0), (0, 0, 1)), parameters)
-    return atoms
+def create_cube(num_layers: "int", kind: "str" = "Cu") -> "ase.Atoms":
+    """
+    Creates an FCC cube with faces on the {100} family of planes.
+
+    :param num_layers: Number of unit cells along each side of the cube.
+    :type num_layers: int
+    :param kind: The element making up the skeleton. Defaults to "Cu"
+    :type kind: str
+
+    :return: An ASE atoms object containing the cube skeleton
+    """
+
+    lattice = ase.lattice.cubic.FaceCenteredCubic(kind, size=[num_layers]*3)
+    cube = ase.build.cut(lattice, extend=1.01)
+    return cube
 
 
-def create_sphere(parameters):
-    atoms = ase.cluster.FaceCenteredCubic('Cu', ((1, 0, 0), (0, 1, 0), (0, 0, 1)), parameters)
+def create_sphere(num_layers: "int", kind: "str" = "Cu") -> "ase.Atoms":
+    """
+    Inscribes a sphere inside a cube and makes it a nanoparticle. Perfect symmetry not guaranteed.
+
+    :param num_layers: The size of the lattice containing the inscribed sphere.
+    :type num_layers: int
+    :param kind: The element making up the skeleton. Defaults to "Cu"
+    :type kind: str
+
+    :return: An ASE atoms object containing the sphere skeleton.
+    """
+
+    atoms = create_cube(num_layers, kind)
+
     cen = atoms.positions.mean(0)
-    radius = (parameters[0] - 1) * 3.61 / 2
-    atoms.append(ase.Atom("Cu", cen))
-    # Filter
-    # 1st arg is a function returning T or F
-    #    Check_distance needs 3 args: atoms object, radius, and a count
-    # The rest of the arguments are lists of things that get plugged in
 
-    my_cluster = 0#ase.Atoms(filter(lambda x: check_distance(atoms, radius, x.index), atoms))
+    radius = (num_layers - 1) * 3.61 / 2
+
+    atoms.append(ase.Atom(kind, cen))
+
+
+    my_cluster = 0  ase.Atoms(filter(lambda x: check_distance(atoms, radius, x.index), atoms))
     return my_cluster
