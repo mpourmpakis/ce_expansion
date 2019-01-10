c26b4d3284594e36e8d4ce3ae9b0c42204b80a6d
michael-cowan <mcowan92@gmail.com>
2019-01-10 12:42:14 -0500

    cleaned up ga code (mainly removed blocks of test code in 'if __name__...' section

--- src/ga.py
+++ src/ga.py
@@ -31,19 +31,17 @@ class Chromo(object):
         if n_dope > self.n_atoms:
             raise ValueError("Can't dope more atoms than there are atoms...")
 
+        # if an array is given, use it - else random generate array
         if isinstance(arr, np.ndarray):
             self.arr = arr
             self.n_dope = arr.sum()
         else:
-            # self.arr = np.array(find_min(atomg, n_dope, return_n=1)[0])
-            # self.arr[random.sample(range(self.n_atoms), n_dope)] = 1
             self.arr[:n_dope] = 1
             np.random.shuffle(self.arr)
 
         # calculate initial CE
         self.calc_score()
 
-    # create getAtomicCE method
     def get_atomic_ce(self, i):
         return self.atomg.getAtomicCE(i, self.arr)
 
@@ -101,23 +99,13 @@ class Chromo(object):
 
 class Pop(object):
     def __init__(self, atomg, n_dope=1, popsize=100, kill_rate=0.2,
-                 mate_rate=0.25, mute_rate=0.1, mute_num=1, x_dope=None,
-                 check4min=True):
+                 mate_rate=0.25, mute_rate=0.1, mute_num=1, x_dope=None):
         self.atomg = atomg
         self.popsize = popsize
         if x_dope:
             self.n_dope = int(atomg.n_atoms * x_dope)
         else:
             self.n_dope = n_dope
-
-        # build random population
-        self.min_found = False
-        self.check4min = check4min
-        self.build_pop()
-
-        self.sort_pop()
-        self.info = []
-        self.stats()
 
         self.nkill = int(popsize * kill_rate)
         self.nmut = int((popsize - self.nkill) * mute_rate)
@@ -126,49 +114,31 @@ class Pop(object):
         self.mute_rate = mute_rate
         self.kill_rate = kill_rate
 
+        self.initialize_new_run()
+
+    def initialize_new_run(self):
+        self.build_pop()
+        self.sort_pop()
+
+        self.info = []
+        self.stats()
+
         # track runtime
         self.runtime = 0
 
-    def build_pop(self, rand=False):
-        if rand:
-            
-            # add 1 of max CN filled
-
-            self.pop = [Chromo(self.atomg, n_dope=self.n_dope) for i in range(self.popsize - 2)]
-            return
-        self.pop = []
-        if self.check4min:
-            alg1 = find_min(self.atomg, self.n_dope)
-            self.pop.append(Chromo(self.atomg, n_dope=self.n_dope, arr=alg1[0]))
-            if alg1[2]:
-                self.min_found = True
-                return
-            else:
-                pops = self.popsize - 1
-        else:
-            pops = self.popsize
-
-        self.pop += [Chromo(self.atomg, n_dope=self.n_dope, arr=z)
-                     for z in find_min(self.atomg, self.n_dope,
-                                       return_n=pops)]
-
-        """
-        if self.check4min:
-            alg1 = find_min(self.atomg, self.n_dope)
-            if alg1[2]:
-                self.pop = [Chromo(self.atomg, n_dope=self.n_dope, arr=alg1[0])]
-                self.min_found = True
-                return
+    def build_pop(self, n_fill_cn=0):
         self.pop = [Chromo(self.atomg, n_dope=self.n_dope)
-                    for i in range(self.popsize)]
-        """
+                    for i in range(self.popsize - n_fill_cn)]
+        self.pop += [Chromo(self.atomg, n_dope=self.n_dope, arr=z)
+                     for z in fill_cn(self.atomg, self.n_dope,
+                                      return_n=n_fill_cn)]
 
     def get_min(self):
         return self.info[:, 0].min()
 
     def step(self, rand=False):
         if rand:
-            self.build_pop()
+            self.build_pop(n_fill_cn=0)
         else:
             self.pop = self.pop[:self.nkill]
 
@@ -196,19 +166,19 @@ class Pop(object):
         self.sort_pop()
         self.stats()
 
-    def run(self, nsteps, std_cut=0, rand=False):
-        start = time.time()
-        i = 0
-        if not self.min_found:
+    def run(self, nsteps=50, std_cut=0, rand=False):
+        # no GA required for monometallic systems
+        if self.n_dope not in [0, self.atomg.n_atoms]:
+            start = time.time()
             for i in range(int(nsteps)):
                 print('\tMin: %.5f eV \t %i' % (self.info[-1][0], i), end='\r')
                 self.step(rand)
                 # if STD less than std_cut end the GA
                 if self.info[-1][-1] < std_cut:
                     break
-        print('\tMin: %.5f eV \t %i' % (self.info[-1][0], i + 1), end='\r')
+            print('\tMin: %.5f eV \t %i' % (self.info[-1][0], i + 1), end='\r')
+            self.runtime = time.time() - start
         self.info = np.array(self.info)
-        self.runtime = time.time() - start
 
     def sort_pop(self):
         self.pop = sorted(self.pop,
@@ -221,19 +191,22 @@ class Pop(object):
                           s.std()])
 
 
-def results_str(pop, disp=True):
+def results_str(p, disp=True):
     res = ' Min: %.5f\nMean: %.3f\n STD: %.3f\n' % tuple(p.info[-1, :])
     res += 'Mute: %.2f\nKill: %.2f\n' % (p.mute_rate, p.kill_rate)
     res += ' Pop: %i\n' % p.popsize
     res += 'nRun: %i\n' % max_runs
-    res += 'Form: %s%i_%s%i\n' % (metal1, len(atom) - n_dope, metal2, n_dope)
+    res += 'Form: %s%i_%s%i\n' % (metal1, len(atom) - p.n_dope,
+                                  metal2, p.n_dope)
     res += 'Done: %i\n' % (len(p.info) - 1) + '\n\n\n'
     if disp:
         print(res.strip('\n'))
     return res
 
 
-def log_results(results):
+def log_results(p):
+    results = results_str(p, disp=False)
+
     with open('results.txt', 'a') as fid:
         fid.write(results)
 
@@ -248,15 +221,13 @@ def log_results(results):
             bestfid.write(results)
 
 
-def make_plot(pop):
+def make_plot(p):
     fig, ax = plt.subplots(figsize=(7, 7))
 
     ax.fill_between(range(len(p.info)), p.info[:, 1] + p.info[:, 2],
                     p.info[:, 1] - p.info[:, 2], color='lightblue',
                     label='STD')
 
-    # ax.plot(range(len(p.info)), p.info[:, 1] + p.info[:, 2])
-    # ax.plot(range(len(p.info)), p.info[:, 1] - p.info[:, 2])
     ax.plot(p.info[:, 1], color='k', label='MEAN')
     ax.plot(p.info[:, 0], ':', color='k', label='MIN')
     ax.legend()
@@ -294,6 +265,11 @@ def gen_random(atomg, n_dope, n=500):
 
 
 def build_icosahedron(nshell, return_adj=True):
+    # ensure necessary directories exist within local repository
+    if not os.path.isdir('../data/atom_objects/'):
+        os.mkdir('../data/atom_objects/')
+    if not os.path.isdir('../data/atom_objects/icosahedron/'):
+        os.mkdir('../data/atom_objects/icosahedron/')
     apath = '../data/atom_objects/icosahedron/%i.pickle' % nshell
     if os.path.isfile(apath):
         with open(apath, 'rb') as fidr:
@@ -315,10 +291,11 @@ def ncr(n, r):
     return numer//denom
 
 
-def find_min(atomg, n_dope, max_search=50, low_first=True, return_n=None, verbose=False):
+def fill_cn(atomg, n_dope, max_search=50, low_first=True, return_n=None,
+            verbose=False):
     formula = 'Cu(%i)Au(%i)' % (atomg.n_atoms - n_dope, n_dope)
 
-    # handle monometallic efficiently
+    # handle monometallic cases efficiently
     if not n_dope or n_dope == atomg.n_atoms:
         struct_min = np.zeros(atomg.n_atoms) if \
             not n_dope else np.ones(atomg.n_atoms)
@@ -391,9 +368,7 @@ def find_min(atomg, n_dope, max_search=50, low_first=True, return_n=None, verbos
                 n_dope -= len(spots)
     if not ce:
         ce = atomg.getTotalCE(struct_min)
-    # print(formula)
-    # print('Min CE: %.3f eV' % ce)
-    return struct_min, ce, checkall
+    return struct_min, ce
 
 
 def make_3d_plot(path, title=None):
@@ -422,44 +397,44 @@ def make_3d_plot(path, title=None):
 
 
 if __name__ == '__main__':
+    # clear previous plots and define desktop and Box paths
     plt.close('all')
-    desk = os.path.expanduser('~') + '/desktop/'
-    box = os.path.expanduser('~') + '\\Box Sync\\Michael_Cowan_PhD_research\\' \
-        'data\\np_ce\\icosahedron\\'
-    # metal1 = 'Cu'
-    # metal2 = 'Au'
+    desk = os.path.expanduser('~') + '\\desktop\\'
+    box = os.path.expanduser('~') + '\\Box Sync\\' \
+        'Michael_Cowan_PhD_research\\data\\np_ce\\icosahedron\\'
+
+    # choose two metals for system
     metal1 = 'Cu'
     metal2 = 'Au'
 
+    # GA properties
     max_runs = 50
-    pop = 100
+    popsize = 100
     kill_rate = 0.2
     mate_rate = 0.8
     mute_rate = 0.2
     mute_num = 1
 
-    # number of times GA runs for a system
-    n_its = 2
-
+    # dict giving number of atoms in icosahedron NP based on nshells
     with open('../data/ico_shell2numatoms.pickle', 'rb') as fidr:
         shell2atoms = pickle.load(fidr)
 
+    # CEs for monometallic icosahedron NPs
     with open('../data/monomet_CE/icosahedron.pickle', 'rb') as fidr:
         icos = pickle.load(fidr)
 
-    # choose number of shells in icosahedron
-    # 24 shells = 10 nm
     eedata = []
     cedata = []
     comps = []
     tot_size = []
-    nshells = 24
 
+    # initialize 3D plot
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     colormap = plt.get_cmap('coolwarm')
 
-    for nshells in range(2, 10):  # 14):
+    # 24 shells = about 10 nm
+    for nshells in range(2, 4):  # 14):
         natoms = shell2atoms[nshells]
         path = 'CuAu/icosahedron/%i/' % natoms
         if not os.path.isdir(desk + path):
@@ -467,27 +442,40 @@ if __name__ == '__main__':
             os.mkdir(desk + path + 'plots')
             os.mkdir(desk + path + 'structures')
 
-        x = np.linspace(0, 1, 21)
+        # x = metal2 concentration [0, 1]
+        x = np.linspace(0, 1, 11)
         n = (x * natoms).astype(int)
 
-        n = np.arange(0, natoms + 1)
-        x = n / n.max()
+        # USE THIS TO TEST EVERY CONCENTRATION
+        # n = np.arange(0, natoms + 1)
+        # x = n / n.max()
 
         rands = np.zeros((len(x), 3))
         ces = np.zeros(len(x))
         atom, adj = build_icosahedron(nshells)
         ag = AtomGraph(adj, metal1, metal2)
 
+        # INITIALIZE POP object
+        pop = Pop(ag, n_dope=n[0], popsize=popsize,
+                  kill_rate=kill_rate, mate_rate=mate_rate,
+                  mute_rate=mute_rate, mute_num=mute_num)
+
         print('%s %s in %i atom Icosahedron' % (metal1, metal2, natoms))
 
         for i, dope in enumerate(n):
-            ces[i] = gen_random(ag, dope, 5)[0]  # find_min(ag, dope, max_search=1)[1]
+            if i:
+                pop.n_dope = dope
+                pop.initialize_new_run()
+            pop.run(max_runs)
+            ces[i] = pop.get_min()
             if dope == 0 and metal1 not in icos[natoms]:
                 icos[natoms][metal1] = ces[i]
                 print('Adding %s for %i atom icosahedron' % (metal1, natoms))
             if dope == natoms and metal2 not in icos[natoms]:
                 icos[natoms][metal2] = ces[i]
                 print('Adding %s for %i atom icosahedron' % (metal2, natoms))
+
+        # calculate excess energy (ees)
         ees = ces - (x * icos[len(atom)][metal2]) - \
             ((1 - x) * icos[len(atom)][metal1])
 
@@ -495,188 +483,12 @@ if __name__ == '__main__':
         tot_size += [atom.cell[0][0] / 10] * len(x)
         cedata += list(ces)
         eedata += list(ees)
-        # cedata.append([natoms, size[0]] + list(ces))
-        # eedata.append([natoms, size[0]] + list(ees))
 
     normalize = matplotlib.colors.Normalize(vmin=min(eedata), vmax=max(eedata))
     ax.plot_trisurf(comps, tot_size, eedata,
-                    cmap=colormap, norm=normalize, anitialized=True)
+                    cmap=colormap, norm=normalize)
     ax.set_xlabel('$X_{%s}$' % metal2)
     ax.set_ylabel('Size (nm)')
     ax.set_zlabel('EE (eV)')
     ax.set_title('%s %s Icosahedron' % (metal1, metal2))
     fig.show()
-    sys.exit()
-    """
-    cols = ['n_atoms', 'diameter'] + ['%i%%_%s' % (w, metal2)
-                                      for w in np.arange(11) * 10]
-
-    excel = box + 'icosahedron_%s%s_data_sub5.xlsx' % (metal1, metal2)
-
-    cedf = pd.DataFrame(cedata, columns=cols)
-    eedf = pd.DataFrame(eedata, columns=cols)
-
-    writer = pd.ExcelWriter(excel, engine='xlsxwriter')
-    cedf.to_excel(writer, sheet_name='CE Data', index=False)
-    eedf.to_excel(writer, sheet_name='EE Data', index=False)
-    writer.save()
-    writer.close()
-    """
-
-    sys.exit()
-    # ga = np.load('../data/CuAu_%i_icosahedron_data.npy' % (shell2atoms[nshells]))
-    # aa = np.array([find_min(ag, i)[1] for i in range(ag.n_atoms + 1)])
-    # plt.plot(ga[:, 0], aa, label='alg1')
-    # plt.plot(ga[:, 0], ga[:, 2], label='GA')
-    # plt.xlabel('n')
-    # plt.ylabel('CE (eV)')
-    # plt.legend()
-    # plt.title('Icosahedron: $Cu_{%i-n}\\ Au_n$' % ag.n_atoms)
-    # plt.show()
-    # plt.savefig(desk + 'ico_%i.png' % ag.n_atoms, dpi=900)
-    minfound = []
-    maybemin = []
-    for i, n_dope in enumerate(n):
-        same = 0
-        ce = 0
-        min_struct = None
-        x_dope = x[i]
-
-        # randomly guess minimum 'n' times
-        rands[i, :] = gen_random(ag, n_dope, n=50)
-        print('X = %.2f, Doped %i / %i' % (x_dope, n_dope, natoms))
-        check4min = True
-        for j in range(n_its):
-            if j:
-                check4min = False
-            p = Pop(ag, n_dope=n_dope, popsize=pop, mate_rate=mate_rate,
-                    mute_rate=mute_rate, kill_rate=kill_rate,
-                    mute_num=mute_num, check4min=check4min)
-            p.run(max_runs)
-
-            if p.min_found:
-                ce = p.get_min()
-                min_struct = p.pop[0]
-                minfound.append(i)
-                break
-            else:
-                maybemin.append(i)
-
-            # if the same minimum is found 3 times in a row
-            # assume the minimum has been found and break loop
-            if abs(ce - p.get_min()) < 1e-4:
-                same += 1
-                print(same)
-            else:
-                same = 0
-            if ce > p.get_min():
-                ce = p.get_min()
-                min_struct = p.pop[0]
-            if same == 3:
-                break
-            print(' ' * 50, end='\r')
-
-        ces[i] = ce
-        formula = metal1 + str(natoms - n_dope) + metal2 + str(n_dope)
-        # make_xyz(atom, min_struct,
-        #         desk + path + 'structures/%s.xyz' % formula)
-        print('Min CE: %.5f eV' % ce)
-
-    # excess energy
-    ees = ces - (x * icos[len(atom)][metal2]) - \
-        ((1 - x) * icos[len(atom)][metal1])
-
-    randees = rands[:, 0] - (x * icos[len(atom)][metal2]) - \
-        ((1 - x) * icos[len(atom)][metal1])
-
-    # Save data
-    # number doped, percent doped, random min CE, GA min CE, EE
-    data = np.zeros((len(x), 5))
-    data[:, 0] = n
-    data[:, 1] = x
-    data[:, 2] = rands[:, 0]
-    data[:, 3] = ces
-    data[:, 4] = ees
-    # np.save('../data/CuAu_%i_icosahedron_data.npy' % natoms, data)
-
-
-
-    # CE plot
-    f1, a1 = plt.subplots()
-    a1.plot(n, rands[:, 0], 'x', label='Random', color='k')
-    a1.plot(n[maybemin], ces[maybemin], 'o', label='GA',
-            color='lightblue', markeredgecolor='k')
-    a1.plot(n[minfound], ces[minfound], 'o', label='Brute Force Min',
-            color='orange', markeredgecolor='k')
-    a1.legend()
-    a1.set_xlabel('$n$')
-    a1.set_ylabel('CE (eV)')
-    a1.set_title('Cohesive Energy, Icosahedron: '
-                 '$%s_{%i-n}\\ %s_n$' % (metal1, len(atom), metal2))
-    f1.tight_layout()
-    f1.savefig(desk + path + 'plots/%s%s_ce.png' % (metal1, metal2),
-               dpi=900)
-
-    # excess energy plot
-    f2, a2 = plt.subplots()
-    a2.plot(n, randees, 'x', label='Random', color='k')
-    a2.plot(n[maybemin], ees[maybemin], 'o', label='GA',
-            color='lightblue', markeredgecolor='k')
-    a2.plot(n[minfound], ees[minfound], 'o', label='Brute Force Min',
-            color='orange', markeredgecolor='k')
-    a2.set_xlabel('$n$')
-    a2.set_ylabel('Excess Energy, EE (eV)')
-    a2.set_title('Excess Energy, Icosahedron: '
-                 '$%s_{%i-n}\\ %s_n$' % (metal1, len(atom), metal2))
-    a2.legend()
-    f2.tight_layout()
-    f2.savefig(desk + path + 'plots/%s%s_excessenergy.png' % (metal1, metal2),
-               dpi=900)
-    plt.show()
-
-    """
-    # f, a = make_plot(p)
-    # plt.show()
-    # tot_rts = np.array(tot_rts)
-    # tot_mins = np.array(tot_mins)
-
-
-    plt.close('all')
-    fig, ax = plt.subplots(2, 1, sharex=True)
-    ax1, ax2 = ax
-
-    ax1.errorbar(x, tot_rts[:, 1], yerr=tot_rts[:, 2], color='blue',
-                 label='Runtimes (s)', ecolor='k', marker='o',
-                 capsize=5, markeredgecolor='k', linestyle='')
-
-    ax1.plot(x, tot_rts[:, 0], 'x', color='k')
-    # ax1.plot(x, rts, color='red', label='Runtimes (s)')
-    ax1.set_ylabel('Runtime for %i Runs (s)' % max_runs)
-
-    ax2.errorbar(x, tot_mins[:, 1], yerr=tot_mins[:, 2], color='red',
-                 label='GA', ecolor='k', marker='o',
-                 capsize=5, markeredgecolor='k', linestyle='')
-
-    ax2.plot(x, tot_mins[:, 0], 'x', color='k')
-
-    # plot results from random iterations
-    ax2.errorbar(0, rand_mins[1], yerr=rand_mins[2], color='gold',
-                 label='Random', ecolor='k', marker='o',
-                 capsize=5, markeredgecolor='k', linestyle='')
-
-    ax2.plot(0, rand_mins[0], 'x', color='k')
-
-    # ax2.plot(x, mins, color='blue', label='CE (eV)')
-    ax2.set_ylabel('Minimum CE (eV)')
-    ax2.set_xlabel(xlabel)
-    ax2.legend()
-    # ax2.axes.invert_yaxis()
-    # fig.legend(loc='upper center')
-    fig.tight_layout()
-    fig.savefig(desk + '/rt_%s_%i_dope%i.png' % (xlabel.replace(' ',
-                                                                '').lower(),
-                                                 pop, n_dope))
-    fig.show()
-    # res = results_str(p)
-    # log_results(res)
-    """
