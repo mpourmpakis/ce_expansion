8a214294b7497cf0a69adb5082223b73afb6cd3b
AcylSilane <jrd101@pitt.edu>
2019-01-31 17:59:16 -0500

    Debug off-by-one in lib.c throwing off array inexing

--- src/lib.c
+++ src/lib.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 const long int num_elements = 2;
-const long int max_coordination = 12;
+const long int max_coordination = 13;
 
 long int fib(long int a);
 
@@ -18,14 +18,50 @@ double calculate_ce(double bond_energies[num_elements][num_elements][max_coordin
     // Loop over the bond system
     double cohesion = 0;
     long int i=0;
-    for (i; i < num_bonds; i++){
+	
+    /* Debugging information
+	printf("Printing bond energies\n");
+    for (i=0; i < 2*2*13; i++){
+		printf("Element %d, " , i);
+        printf("CN %d: %f\n", i%13, bond_energies[0][0][i]);
+    }
+	printf("Printing the table way\n");
+	for (i=0; i < 2; i++){
+		int j = 0;
+		for (j=0; j < 2; j++){
+			int k = 0;
+			for (k = 0; k < 13; k++){
+				printf("Index (%d, %d), CN %d: %f\n", i, j, k, bond_energies[i][j][k]);
+			}
+		}
+        
+    }
+	return 0;
+	
+	for(i=0; i < num_atoms; i++){
+		printf("Atom %d: Coordination %d\n", i, cns[i]);
+	}
+	*/
+
+	
+    for (i=0; i < num_bonds; i++){
+		//printf("Bond %d, ", i);
         long int bond_source = id_array[adj_table[i][0]];
+		//printf("Source kind = %d, ", bond_source);
         long int bond_destination = id_array[adj_table[i][1]];
+		//printf("Source destination = %d\n", bond_destination);
         long int coordination = cns[adj_table[i][0]];
+		//printf("Coordination = %d, ", coordination);
 
         // Add the bond energy to the running total
+		double contribution = bond_energies[bond_source][bond_destination][coordination];
         cohesion += bond_energies[bond_source][bond_destination][coordination];
+		
+		// Print debug stuff
+		//printf("bond energy = %f\n", contribution);
+		//printf("Total cohesion is now: %f\n", cohesion);
     }
     cohesion /= num_atoms;
+	//printf("Dividing by %d atoms, resulting in %f\n", num_atoms, cohesion);
     return cohesion;
 }
