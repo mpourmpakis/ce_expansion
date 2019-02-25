20a7ba223234ef1d35d1d13f00b9095e5190f905
michael-cowan <mcowan92@gmail.com>
2019-02-25 14:16:27 -0500

    batch_ga_run now creates new_struct_summary figure in data path after completing a logged batch GA run

--- src/batch_ga_run.py
+++ src/batch_ga_run.py
@@ -1,5 +1,7 @@
 import ga
 import os
+import numpy as np
+import matplotlib.pyplot as plt
 
 """
        Main script to run a batch submission job for GA sims
@@ -17,6 +19,9 @@ metal_opts = [('Ag', 'Cu'),
 shape_opts = ['icosahedron', 'fcc-cube', 'cuboctahedron',
               'elongated-pentagonal-bipyramid']
 
+# log batch GA results
+log = True
+
 # start batch GA run
 batch_tot = len(metal_opts) * len(shape_opts)
 batch_i = 1
@@ -26,7 +31,51 @@ for metals in metal_opts:
                   datapath=datapath,
                   save_data=True,  # True,
                   plotit=False,
-                  log_results=True,  # True,
+                  log_results=log,
                   add_current_min=True,
                   batch_runinfo='%i of %i' % (batch_i, batch_tot))
         batch_i += 1
+
+# if results were logged, update new structures plot in <datapath>
+if log:
+    struct_info = {}
+    z = 0
+    for metals in metal_opts:
+        for shape in ['Icos', 'FCC-Cube', 'Cuboct', 'J16']:
+            struct_info[z] = '%s: %s' % (''.join(metals), shape)
+            z += 1
+
+    cs = []
+    pcts = []
+    with open(os.path.join(datapath, 'sims.log'), 'r') as fidr:
+        for i in fidr:
+            if 'New Min Structs' in i:
+                cs.append(int(i.split()[-1].strip('\n')))
+            elif '% New Structs' in i:
+                pcts.append(float(i.split()[-1].strip('\n')[:-1]) / 100)
+
+    cs = np.array(cs).reshape(int(len(cs) / z), z).T
+    pcts = np.array(pcts).reshape(int(len(pcts) / z), z).T
+
+    x = range(1, cs.shape[1] + 1)
+
+    f, a = plt.subplots(2, 1, sharex=True, figsize=(7, 7))
+    a1, a2 = a
+    for i in range(len(cs)):
+        colors = plt.cm.tab20c((i / float(z)) * 0.62)
+        a1.plot(x, cs[i], '.-', linewidth=1, color=colors,
+                label=struct_info[i])
+        a2.plot(x, pcts[i], '.-', linewidth=1, color=colors)
+
+    # change ax2's y-axis to percent
+    vals = a2.get_yticks()
+    a2.set_yticklabels(['{:,.0%}'.format(x) for x in vals])
+
+    a1.legend(ncol=3)
+    a1.set_title('New Minimum Structures Found')
+    a1.set_ylabel('Total Count')
+    a2.set_ylabel('Percentage')
+    a2.set_xlabel('Day')
+    f.tight_layout()
+    f.savefig(os.path.join(datapath, 'new_struct_summary.png'))
+    # f.savefig(os.path.join(datapath, 'new_struct_summary.svg'))
