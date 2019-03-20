c33330261e45f3b4faf5c7ba9ac58b87e559329c
michael-cowan <mcowan92@gmail.com>
2019-03-20 13:56:09 -0400

    batch ga runs 4 metal options per day (out of 28 total) such that all metal options are run once a week

--- src/batch_ga_run.py
+++ src/batch_ga_run.py
@@ -1,5 +1,7 @@
 import ga
 import os
+import itertools
+from datetime import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 from npdb import db_inter
@@ -8,25 +10,32 @@ from npdb import db_inter
        Main script to run a batch submission job for GA sims
        - runs multiple ga sims sweeping size, shape, and composition
        - creates/updates "new structures found" summary plot based on sims.log
+
+       NOTE: Currently runs 4 metal combinations and 5 shapes over 1 iteration
+             - should take a little over 17 hours
 """
 
 datapath = os.path.join(os.path.expanduser('~'), 'Box Sync',
                         'Michael_Cowan_PhD_research', 'data', 'np_ce')
 
 # all metal options
-metals = db_inter.build_metals_list()
+# 28 total options
+metals = list(itertools.combinations(db_inter.build_metals_list(), 2))
 
-# Current metal combinations being studied
-metal_opts = [('Ag', 'Cu'),
-              ('Ag', 'Au'),
-              ('Au', 'Cu')
-              ]
+
+# run 4 metal options a day to iterate once a week through all options
+# e.g. (Saturday = 5) * 4 = 20
+day = datetime.now().weekday()
+start_index = day * 4
+
+# chooses 4 metals from list of 28
+metal_opts = metals[start_index:start_index + 4]
 
 shape_opts = ['icosahedron', 'fcc-cube', 'cuboctahedron',
               'elongated-pentagonal-bipyramid']
 
 # HOW MANY TIMES THE TOTAL BATCH RUN SHOULD REPEAT
-niterations = 2
+niterations = 1
 
 # start batch GA run
 batch_tot = len(metal_opts) * len(shape_opts)
@@ -42,4 +51,4 @@ for n in range(niterations):
 
 # update new structures plot in <datapath>
 fig = db_inter.build_new_structs_plot(metal_opts, shape_opts)
-fig.savefig(os.path.join(datapath, 'new_struct_summary.png'))
+fig.savefig(os.path.join(datapath, '%02i_new_struct_log.png' % day))
