1b0284a7f2beffe06e439dfdd2c54acd4cdd347b
AcylSilane <jrd101@pitt.edu>
2019-03-28 16:32:16 -0400

    Add monte-carlo optimization to atomgraph
    Signed-off-by: AcylSilane <jrd101@pitt.edu>

--- src/atomgraph.py
+++ src/atomgraph.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 import ctypes
-import pickle
-import os
+
+import numpy as np
 
 import interface
-import numpy as np
 from npdb import db_inter
 
 
@@ -36,6 +35,7 @@ class AtomGraph(object):
     num_atoms (int): The number of atoms in the NP.
     cns (np.array): An array containing the coordination number of each atom.
     """
+
     def __init__(self, bond_list: "np.array", kind0: "str", kind1: "str"):
 
         self._bond_list = bond_list.astype(ctypes.c_long)
@@ -56,7 +56,7 @@ class AtomGraph(object):
 
         # Create pointers
         self._long_num_atoms = ctypes.c_long(self.num_atoms)
-        self._p_cns = self.cns.ctypes.data_as(ctypes.POINTER(ctypes.c_long)) # Windows compatibility?
+        self._p_cns = self.cns.ctypes.data_as(ctypes.POINTER(ctypes.c_long))  # Windows compatibility?
         self._long_num_bonds = ctypes.c_long(self._num_bonds)
         self._p_bond_list = self._bond_list.ctypes.data_as(ctypes.POINTER(ctypes.c_long))
 
@@ -99,6 +99,86 @@ class AtomGraph(object):
                                                   self._long_num_bonds,
                                                   self._p_bond_list,
                                                   p_ordering)
+
+    def monte_carlo_movement(self, initial_ordering,
+                             num_steps=1000, accept_good_rate=0.8,
+                             decline_bad_rate=0.8):
+        '''
+        Monte-carlo simulation of atomic diffusion in the NP
+
+        Args:
+        atomgraph (atomgraph.AtomGraph) : An atomgraph representing the NP
+        initial_ordering (np.array) : 1D chemical ordering array
+        kinds (tuple) : Tuple. Index0 is the element a 0 represents. Same for 1 and Index1.
+        num_steps (int) : How many steps to simulate for
+        accept_good_rate (float) : How often a step lowering the energy is accepted. 0=0%, 1=100%.
+            A 1 accepts all "good" steps, and leads towards the local minimum.
+            A 0 accepts no "good" steps, and leads to the system only increasing its energy.
+        decline_good_rate (float) : How often a step increasing the energy is declined.
+            A 1 decline all "bad" steps, and only allows movement to the local minimum.
+            A 0 accepts all "bad" steps, and leads to the system only increasing its energy.
+
+        '''
+
+        step = 0
+        num_rerolls = 0
+        adj_list = self.get_adjacency_list()
+        ordering = initial_ordering
+        energy = self.getTotalCE(ordering)
+        energy_history = np.zeros[num_steps]
+        while step < num_steps:
+            prev_ordering = ordering
+            prev_energy = energy
+            # Choose random atom in the NP
+            moved_atom = np.random.choice([0, len(initial_ordering)])
+
+            # Choose the atom to swap with
+            destination = np.random.choice([0, len(adj_list[moved_atom])])
+
+            # Swap them
+            if ordering[destination] == ordering[moved_atom]:
+                # They are the same
+                num_rerolls += 1
+                if num_rerolls < 1000:
+                    # To make the simulation go faster, try to force it to swap
+                    # If no swaps happen after a while, let it be
+                    continue
+                else:
+                    energy_history[step] = prev_energy
+                    step += 1
+
+            else:
+                # They are different
+                num_rerolls = 0
+                # 1-x flips between 0 and 1
+                ordering[destination] = 1 - ordering[destination]
+                ordering[moved_atom] = 1 - ordering[moved_atom]
+
+                # Check the energy
+                energy = self.getTotalCE(ordering)
+                dice_roll = np.random.uniform()
+                if energy < prev_energy:
+                    # Step reduces the energy
+                    if dice_roll < accept_good_rate:
+                        # Step is accepted
+                        energy_history[step] = energy
+                    else:
+                        # Step is declined
+                        energy_history[step] = prev_energy
+                        energy = prev_energy
+                        ordering = prev_ordering
+                else:
+                    # Step increases the energy
+                    if dice_roll < decline_bad_rate:
+                        # Step is declined
+                        energy_history[step] = prev_energy
+                        energy = prev_energy
+                        ordering = prev_ordering
+                    else:
+                        # Step is accepted
+                        energy_history[step] = energy
+                step += 1
+        return ordering, energy_history
 
 
 if __name__ == '__main__':
