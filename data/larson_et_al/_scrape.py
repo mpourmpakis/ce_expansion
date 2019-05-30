dd4ba5db8e58761c8c9bccb178096d760dfea71a
AcylSilane <jrd101@pitt.edu>
2019-05-30 18:28:32 -0400

    Correctly perform scrape

--- data/larson_et_al/_scrape.py
+++ data/larson_et_al/_scrape.py
@@ -2,35 +2,45 @@
 
 import sys
 
+import ase.io
 import requests
 
 #####
 sys.exit("Delete this line to re-run database scrape")
 #####
 
-def pullRecord(filename, address):
- """
- A record from the database presented in the following article:
-   Larson, P. M.; Jacobsen, K. W.; Schiotz, J. Rich Ground-state chemical
-   Ordering in Nanoparticles: Exact Solution of a Model for Ag-Au Clusters.
-   Phys. Rev. Lett. 2018. 120, 256101.
-   DOI: 10.1103/PhysRevLett.120.256101
- Database URL:
-   https://cmrdb.fysik.dtu.dk/agau309
+def pullRecord(address):
+    """
+    A record from the database presented in the following article:
+      Larson, P. M.; Jacobsen, K. W.; Schiotz, J. Rich Ground-state chemical
+      Ordering in Nanoparticles: Exact Solution of a Model for Ag-Au Clusters.
+      Phys. Rev. Lett. 2018. 120, 256101.
+      DOI: 10.1103/PhysRevLett.120.256101
+    Database URL:
+      https://cmrdb.fysik.dtu.dk/agau309
 
- Args:
-   filename (str): The filename that will be written
-   address (str): The URL of the XYZ file
- """
- print("Sending GET request to " + address)
- response = requests.get(address)
- record = response.content.decode("utf-8")
- print("Successful. Writing record to " + filename)
- with open(filename, "w") as outp:
-   for line in record:
-     outp.write(line)
+    Args:
+       address (str): The URL of the XYZ file
+    """
+    print("Sending GET request to " + address)
+    response = requests.get(address)
+    record = response.content.decode("utf-8")
 
-for i in range(0,310):
- filename = "Ag" + str(309-i) + "Au" + str(i) + ".xyz"
- address = "https://cmrdb.fysik.dtu.dk/agau309/xyz/" + str(i)
- pullRecord(filename, address)
+    temp = open("tempfile", "w")
+
+    for line in record:
+        temp.write(line)
+
+    temp.seek(0)
+    atoms = ase.io.read("tempfile")
+
+    filename = atoms.get_chemical_formula() + ".xyz"
+    print("Successful. Writing record to " + filename)
+    ase.io.write(filename, atoms)
+
+    temp.close()
+
+
+for i in range(1, 311):
+    address = "https://cmrdb.fysik.dtu.dk/agau309/xyz/" + str(i)
+    pullRecord(address)
