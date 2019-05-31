3cbaf27fbcddd64870896f2041182c47d4724175
michael-cowan <mcowan92@gmail.com>
2019-05-30 21:21:22 -0400

    x and y axis can be converted to conc. and percentage, resp.

--- src/shell_vs_tot.py
+++ src/shell_vs_tot.py
@@ -14,9 +14,14 @@ for opt in plt.rcParams:
 
 
 def build_nmet2_nmet2shell_plot(metals, shape, num_shells,
-                                show_ee=True, show=True, save=False):
+                                show_ee=True, show=True, save=False,
+                                pctx=False, pcty=False):
     shell_dict = {s: db_inter.build_atoms_in_shell_list(shape, s)
                   for s in range(2, num_shells + 1)}
+
+    # if pct use X else N
+    xtype = 'X' if pctx else 'N'
+    ytype = 'X' if pcty else 'N'
 
     # add central atom to first shell
     shell_dict[2].append(0)
@@ -34,10 +39,17 @@ def build_nmet2_nmet2shell_plot(metals, shape, num_shells,
             nmet2_shell = sum([1 for a in shell_atoms
                                if a.symbol == nanop.metal2])
             results[i, s - 1] = nmet2_shell
+            # calculate as percentage of atoms that are "metal2" in given shell
+            if pcty:
+                results[i, s - 1] = nmet2_shell / len(shell_atoms)
         results[i, s] = nanop.EE
 
-    # ensure shell counts were correctly calculated
-    assert (results[:, 0] == results[:, 1:-1].sum(1)).all()
+    if pctx:
+        results[:, 0] = results[:, 0] / nanop.num_atoms
+
+    if not (pctx or pcty):
+        # ensure shell counts were correctly calculated
+        assert (results[:, 0] == results[:, 1:-1].sum(1)).all()
 
     shell_color = ['gray', 'green', 'red', 'blue', 'cyan', 'gold',
                    'orange', 'purple', 'pink']
@@ -50,7 +62,7 @@ def build_nmet2_nmet2shell_plot(metals, shape, num_shells,
         ax2 = ax.twinx()
         ax2.tick_params(axis='y', labelcolor=ee_color)
         ax2.plot(results[:, 0], results[:, -1], '^', label='EE',
-                color=ee_color, zorder=-100)
+                 color=ee_color, zorder=-100)
         ax2.set_ylabel('Excess Energy (eV / atom)', color=ee_color)
         # ax2.set_ylim(-0.03, 0.03)
 
@@ -60,12 +72,20 @@ def build_nmet2_nmet2shell_plot(metals, shape, num_shells,
                 label='Shell %i' % i, zorder=i)
 
     # used to match Larsen et al.'s plot
-    if num_shells == 5:
+    if num_shells == 5 and not (pcty or pctx):
         ax.set_xlim(0, 315)
         ax.set_ylim(0, 180)
 
-    ax.set_xlabel('$\\rm N_{%s}$ in NP' % nanop.metal2)
-    ax.set_ylabel('$\\rm N_{%s}$ in shell' % nanop.metal2)
+    if pctx:
+        ax.set_xlim(0, 1)
+        # ax.set_xticklabels(['{:,.0%}'.format(x) for x in ax.get_xticks()])
+
+    if pcty:
+        ax.set_ylim(0, 1.15)
+        ax.set_yticklabels(['{:,.0%}'.format(x) for x in ax.get_yticks()])
+
+    ax.set_xlabel('$\\rm %s_{%s}$ in NP' % (xtype, nanop.metal2))
+    ax.set_ylabel('$\\rm %s_{%s}$ in Shell$\\rm _i$' % (ytype, nanop.metal2))
 
     title = '%s atom %s%s %s NP' % (nanop.num_atoms, nanop.metal1,
                                     nanop.metal2, nanop.shape)
@@ -74,25 +94,39 @@ def build_nmet2_nmet2shell_plot(metals, shape, num_shells,
     ax.legend(loc='upper center', ncol=3)
     fig.tight_layout()
     path = 'C:\\Users\\YLA\\Box Sync\\Michael_Cowan_PhD_research\\np_ga\\' \
-        'FIGURES\\nmet_vs_nmetshell-LARSON\\%s%s-%s\\' % (nanop.metal1,
-                                                          nanop.metal2,
-                                                          nanop.shape[:3])
+           'FIGURES\\%smet_vs_%smetshell-LARSON\\%s%s-%s\\' % (xtype, ytype,
+                                                               nanop.metal1,
+                                                               nanop.metal2,
+                                                               nanop.shape[:3])
     if save:
         if not os.path.isdir(path):
             os.mkdir(path)
+        if show_ee:
+            path += 'ee\\'
+            if not os.path.isdir(path):
+                os.mkdir(path)
         fig.savefig(path + title.replace(' ', '_') + '.png')
     if show:
         plt.show()
     return fig, results
 
 if __name__ == '__main__':
-    metals = 'aucu'
+    metals = 'agau'
     shape = 'icosahedron'
+    pctx = False
+    pcty = False
+
+    save = False
+    show = True
+    show_ee = True
+
     shell_range = range(2, 10)
-    for num_shells in [4]:
+    for num_shells in [3]:
         build_nmet2_nmet2shell_plot(metals,
                                     shape,
                                     num_shells,
-                                    show=True,
-                                    save=False,
-                                    show_ee=True)
+                                    show=show,
+                                    save=save,
+                                    show_ee=show_ee,
+                                    pctx=pctx,
+                                    pcty=pcty)
