cbbc25d371917ae29bf564944955dae27d4beb84
michael-cowan <mcowan92@gmail.com>
2019-06-04 17:58:07 -0400

    added tools to write txt files to desktop so I know when batchga task is running/completed

--- src/batch_ga_run.py
+++ src/batch_ga_run.py
@@ -1,9 +1,12 @@
 import ga
 import os
+import time
 import itertools
 from datetime import datetime
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from npdb import db_inter
 
 """
@@ -14,9 +17,14 @@ from npdb import db_inter
        NOTE: Currently runs 4 metal combinations and 5 shapes over 1 iteration
              - should take a little over 17 hours
 """
-
-datapath = os.path.join(os.path.expanduser('~'), 'Box Sync',
+home = os.path.expanduser('~')
+datapath = os.path.join(home, 'Box Sync',
                         'Michael_Cowan_PhD_research', 'data', 'np_ce')
+
+# create text file on desktop to indicate GA is running
+running = os.path.join(home, 'Desktop', 'RUNNING-GA.txt')
+with open(running, 'w') as fid:
+    fid.write('GA is currently running...hopefully')
 
 # all metal options
 # 28 total options
@@ -42,6 +50,9 @@ metal_opts = [('Ag', 'Au'), ('Ag', 'Cu'), ('Au', 'Cu')]
 shape_opts = ['icosahedron', 'fcc-cube', 'cuboctahedron',
               'elongated-pentagonal-bipyramid']
 
+start = time.time()
+startstr = datetime.now().strftime('%Y-%m-%d %H:%M %p')
+
 # start batch GA run
 batch_tot = len(metal_opts) * len(shape_opts)
 for n in range(niterations):
@@ -62,3 +73,10 @@ fig = db_inter.build_new_structs_plot(metal_opts, shape_opts, pct=False,
                                       cutoff_date=cutoff_date)
 # fig.savefig(os.path.join(datapath, '%02i_new_struct_log.png' % day))
 fig.savefig(os.path.join(datapath, 'agaucu_STRUCTS.png'))
+os.remove(running)
+
+runtime = (time.time() - start) / 3600.
+with open(running.replace('RUNNING', 'COMPLETED'), 'w') as fid:
+    fid.write('completed batch GA in %.2f hours.' % runtime)
+    fid.write('\nstarted: ' + startstr)
+    fid.write('\n  ended: ' + datetime.now().strftime('%Y-%m-%d %H:%M %p'))
