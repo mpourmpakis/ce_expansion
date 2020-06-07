831d5e077fc74824556d4ec8fdafcfd87deacb1f
AcylSilane <jrd101@pitt.edu>
2020-06-07 19:23:20 -0400

    Add getLocalCE, a function which calculates the local cohesive energy of an atom. The function signature is similar to getTotalCE, with an added "index" variable to indicate which atom is being studied.

--- ce_expansion/atomgraph/atomgraph.py
+++ ce_expansion/atomgraph/atomgraph.py
@@ -222,7 +222,23 @@ class AtomGraph(object):
         Returns:
             The local cohesive energy of the atom at <index>.
         """
-        return None
+        ordering = ordering.astype(ctypes.c_long)
+        # Make a pointer for the ordering
+        p_ordering = ordering.ctypes.data_as(ctypes.POINTER(ctypes.c_long))
+        # Make a new bond-list, just for the local set of atoms
+        bond_list = self._bond_list[np.any(self._bond_list == index, axis=1)].astype(ctypes.c_long)
+        p_bond_list = bond_list.ctypes.data_as(ctypes.POINTER(ctypes.c_long))
+        # Number of atoms should be 1, so we only account for the atom at <index>
+        long_num_atoms = ctypes.c_long(1)
+        # Finally, we only want to iterate over the bonds in the bond list
+        num_bonds = len(bond_list)
+        long_num_bonds = ctypes.c_long(num_bonds)
+        return interface.pointerized_calculate_ce(self._p_bond_energies,
+                                                  long_num_atoms,
+                                                  self._p_cns,
+                                                  long_num_bonds,
+                                                  p_bond_list,
+                                                  p_ordering)
 
     def getTotalCE(self, ordering):
         """
