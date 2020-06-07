ac16adfecd2c566e8480230a0d277e36cf77e571
AcylSilane <jrd101@pitt.edu>
2020-06-07 06:33:50 -0400

    Improve indentation for readability

--- ce_expansion/bin/lib.c
+++ ce_expansion/bin/lib.c
@@ -92,70 +92,70 @@ double calculate_ce(double bond_energies[num_elements][num_elements][max_coordin
                     long int num_bonds, // Number of bonds in the system
                     long int adj_table [num_bonds][2], // Adjacency table
                     long int id_array[num_atoms]){ // Representing the identity of each element
-  // Calculates the cohesive energy of the system
-  // Loop over the bond system
-  double cohesion = 0;
-  long int i = 0;
+    // Calculates the cohesive energy of the system
+    // Loop over the bond system
+    double cohesion = 0;
+    long int i = 0;
 
 	#ifdef PRINT_DEBUG_INFO
-    long int j = 0;
-    long int k = 0;
-    //Debugging information
-  	printf("Printing bond energies\n");
-    for (i=0; i < 2*2*13; i++){
-    printf("Element %d, " , i);
-    printf("CN %d: %f\n", i%13, bond_energies[0][0][i]);
-    }
-  	printf("Printing the table way\n");
-  	for (i=0; i < 2; i++){
-      for (j=0; j < 2; j++){
-  			for (k = 0; k < 13; k++){
-  				printf("Index (%d, %d), CN %d: %f\n", i, j, k, bond_energies[i][j][k]);
-  			}
-  		}
-    }
+        long int j = 0;
+        long int k = 0;
+        //Debugging information
+        printf("Printing bond energies\n");
+        for (i=0; i < 2*2*13; i++){
+            printf("Element %d, " , i);
+            printf("CN %d: %f\n", i%13, bond_energies[0][0][i]);
+        }
+        printf("Printing the table way\n");
+        for (i=0; i < 2; i++){
+          for (j=0; j < 2; j++){
+                for (k = 0; k < 13; k++){
+                    printf("Index (%d, %d), CN %d: %f\n", i, j, k, bond_energies[i][j][k]);
+                }
+            }
+        }
 
-  	for(i=0; i < num_atoms; i++){
-      printf("Atom %d: Coordination %d\n", i, cns[i]);
-  	}
-  #endif
-
-  for (i=0; i < num_bonds; i++){
-    #ifdef PRINT_DEBUG_INFO
-      // Print bond source/destination/coordination
-      printf("Bond %d, ", i);
-      long int bond_source = id_array[adj_table[i][0]];
-      printf("Source kind = %d, ", bond_source);
-      long int bond_destination = id_array[adj_table[i][1]];
-      printf("Source destination = %d\n", bond_destination);
-      long int coordination = cns[adj_table[i][0]];
-      printf("Coordination = %d, ", coordination);
-
-      // Add the bond energy to the running total
-      double contribution = bond_energies[bond_source][bond_destination][coordination];
-      cohesion += bond_energies[bond_source][bond_destination][coordination];
-
-      // Print debug stuff
-      printf("bond energy = %f\n", contribution);
-      printf("Total cohesion is now: %f\n", cohesion);
-
-    #else
-      // Debug info is not printed
-      long int bond_source = id_array[adj_table[i][0]];
-      long int bond_destination = id_array[adj_table[i][1]];
-      long int coordination = cns[adj_table[i][0]];
-
-      // Add the bond energy to the running total
-      // double contribution = bond_energies[bond_source][bond_destination][coordination];
-      cohesion += bond_energies[bond_source][bond_destination][coordination];
+        for(i=0; i < num_atoms; i++){
+          printf("Atom %d: Coordination %d\n", i, cns[i]);
+        }
     #endif
-  }
 
-  cohesion /= num_atoms;
+    for (i=0; i < num_bonds; i++){
+        #ifdef PRINT_DEBUG_INFO
+          // Print bond source/destination/coordination
+          printf("Bond %d, ", i);
+          long int bond_source = id_array[adj_table[i][0]];
+          printf("Source kind = %d, ", bond_source);
+          long int bond_destination = id_array[adj_table[i][1]];
+          printf("Source destination = %d\n", bond_destination);
+          long int coordination = cns[adj_table[i][0]];
+          printf("Coordination = %d, ", coordination);
 
-  #ifdef PRINT_DEBUG_INFO
-    printf("Dividing by %d atoms, resulting in %f\n", num_atoms, cohesion);
-	#endif
+          // Add the bond energy to the running total
+          double contribution = bond_energies[bond_source][bond_destination][coordination];
+          cohesion += bond_energies[bond_source][bond_destination][coordination];
+
+          // Print debug stuff
+          printf("bond energy = %f\n", contribution);
+          printf("Total cohesion is now: %f\n", cohesion);
+
+        #else
+          // Debug info is not printed
+          long int bond_source = id_array[adj_table[i][0]];
+          long int bond_destination = id_array[adj_table[i][1]];
+          long int coordination = cns[adj_table[i][0]];
+
+          // Add the bond energy to the running total
+          // double contribution = bond_energies[bond_source][bond_destination][coordination];
+          cohesion += bond_energies[bond_source][bond_destination][coordination];
+        #endif
+    }
+
+    cohesion /= num_atoms;
+
+    #ifdef PRINT_DEBUG_INFO
+        printf("Dividing by %d atoms, resulting in %f\n", num_atoms, cohesion);
+    #endif
 
   return cohesion;
 }
