d5ff0485c5f964a15909ce22d6832384785156bd
James <None>
2020-08-04 05:24:06 -0400

    Add function to calculate total gamma coefficient across a range of CNs

--- ce_expansion/data/gamma.py
+++ ce_expansion/data/gamma.py
@@ -2,6 +2,7 @@
 """
 Code dealing with the gamma coefficients
 """
+import numpy as np
 
 
 def calculate_gamma(bde_aa, bde_bb, bde_ab):
@@ -16,6 +17,7 @@ def calculate_gamma(bde_aa, bde_bb, bde_ab):
     :param bde_ab: Energy of the A-B dimer.
     :return: Gamma coefficient for the given system
     """
+    # If A and B are the same, gamma's are always 1
     if bde_aa == bde_bb == bde_ab:
         gamma_a = 1
         gamma_b = 1
@@ -25,5 +27,24 @@ def calculate_gamma(bde_aa, bde_bb, bde_ab):
         gamma_b = (2 * bde_aa - bde_ab) / (bde_aa - bde_bb)
     return gamma_a, gamma_b
 
+
+def calculate_total_gamma(ce_bulk_a, cn_bulk_a, gamma_a, cn_a_max=None):
+    """
+    Precalculates the total gamma for a half-bond coming from A in the A-B bond.
+    :param ce_bulk_a: Bulk cohesive energy of atom A
+    :param cn_bulk_a: Bulk coordination number of atom A
+    :param gamma_a: Gamma coefficient of atom A for the A-B bond
+    :param cn_a_max: Maximum CN for atom A. Defaults to cn_bulk_a if none provided.
+    :return: The total gamma value for the given system
+    """
+    # Initialize
+    if cn_a_max is None:
+        cn_a_max = cn_bulk_a
+    results_dict = {}
+    # Next, map over the CN range, building up the total gamma dictionary
+    for cn in range(0, cn_a_max + 1):
+        total_gamma = (gamma_a * ce_bulk_a / np.sqrt(cn_bulk_a)) / np.sqrt(cn)
+        results_dict[cn] = total_gamma
+    return results_dict
 
 
